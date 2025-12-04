665658f09628f44e97546535d6bb8705d9cbdedf
Pabllo Eduardo <pablloedu@gmail.com>
2025-12-04 10:16:50 -0300

    alterações no settings.py

--- estocai/settings.py
+++ estocai/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!f^=)iwb_r_n=%4=@93rn)j9c3(4o)p+9nd*uj01ev#3q6nw31'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
